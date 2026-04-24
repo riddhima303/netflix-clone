898a32800b74094070ebd39cbc3e676021c27626
Riddhima Gupta <riddhima303@gmail.com>
2026-04-24 22:35:36 +0530

    wire home rows to tmdb request urls

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,18 @@ import Navbar from '../components/Navbar.jsx'
 import Banner from '../components/Banner.jsx'
 import Row from '../components/Row.jsx'
 import Footer from '../components/Footer.jsx'
+import requests from '../api/requests'
 import { getMoviesByIds, movies, rows } from '../data/movies.js'
 import './Home.css'
 
 export default function Home() {
   const heroMovie = movies[0]
+  const rowRequestMap = {
+    trending: requests.fetchTrending,
+    top10: requests.fetchTopRated,
+    action: requests.fetchActionMovies,
+    drama: requests.fetchDocumentaries,
+  }
 
   return (
     <div className="home">
@@ -16,7 +23,12 @@ export default function Home() {
 
         <section className="home__rows">
           {rows.map((r) => (
-            <Row key={r.id} title={r.title} movies={getMoviesByIds(r.movieIds)} />
+            <Row
+              key={r.id}
+              title={r.title}
+              movies={getMoviesByIds(r.movieIds)}
+              fetchUrl={rowRequestMap[r.id]}
+            />
           ))}
         </section>
       </main>
