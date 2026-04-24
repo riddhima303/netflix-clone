eecdd6e30af50459024333f2e5b329ff5b2f1d85
Riddhima Gupta <riddhima303@gmail.com>
2026-04-24 22:43:59 +0530

    use centralized route constants in app router

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,17 @@ import Home from './pages/Home.jsx'
 import MovieDetails from './pages/MovieDetails.jsx'
 import Login from './pages/Login.jsx'
 import Profiles from './pages/Profiles.jsx'
+import PATHS from './routes/paths.js'
 
 function App() {
   return (
     <div className="app-shell">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:movieId" element={<MovieDetails />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profiles" element={<Profiles />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path={PATHS.home} element={<Home />} />
+        <Route path={PATHS.movieDetails} element={<MovieDetails />} />
+        <Route path={PATHS.login} element={<Login />} />
+        <Route path={PATHS.profiles} element={<Profiles />} />
+        <Route path="*" element={<Navigate to={PATHS.home} replace />} />
       </Routes>
     </div>
   )
